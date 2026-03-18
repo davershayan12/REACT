260c62f4b5d962fc78c68c3f174de16fe8f2c515
Daver Shayan <davershayan13@gmail.com>
2026-03-19 00:40:29 +0500

    REACT0

---
+++ Basic thing to learn before REACT/REACT0.js
@@ -0,0 +1,17 @@
+function Something(){
+    return console.log("something");
+}
+
+const something  =()=>{
+    return console.log("something");
+}
+
+var age= 18
+if (age >= 18){
+    console.log("eligible");
+}
+
+
+_if_=age >= 18 && "eligible"
+
+if_not= age >= 18 || "not eligible"
