659a15466f007606d357426d52bcde68135fce11
Daver Shayan <davershayan13@gmail.com>
2026-03-19 11:41:12 +0500

    REACT0

--- Basic thing to learn before REACT/REACT0.js
+++ Basic thing to learn before REACT/REACT0.js
@@ -1,17 +1,32 @@
+//normal function
 function Something(){
     return console.log("something");
 }
 
+//arrow function 
+/* Arrow functions are often considered "better" in modern JavaScript due to their simpler, more concise syntax and more predictable handling of the this keyword. This makes the code cleaner, shorter, and easier to read in most common scenarios. 
+ */
 const something  =()=>{
     return console.log("something");
 }
 
-var age= 18
+
+//normal if
+let age= 18;
+let satement;
 if (age >= 18){
     console.log("eligible");
 }
 
+let _if_=age >= 18 && "eligible"
+console.log(_if_);
 
-_if_=age >= 18 && "eligible"
+if (age >=! 18){
+    console.log("eligible");
+}
 
-if_not= age >= 18 || "not eligible"
+let if_not= age >= 18 || "not eligible"
+console.log(if_not);
+
+let if_else= age >= 18 ? "eligible" : "not eligible"
+console.log(if_else);
