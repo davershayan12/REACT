0f77ebbdd3b9f841b15246ff84dd859907c8e253
Daver Shayan <davershayan13@gmail.com>
2026-03-24 02:35:26 +0500

    .

--- Basic_thing_to_learn_before_REACT/Typescript/src/UserContextProvider.tsx
+++ Basic_thing_to_learn_before_REACT/Typescript/src/UserContextProvider.tsx
@@ -1,9 +1,20 @@
+import { createContext } from "react";
+import { User } from "./Person";
 export interface User {
   name: string;
   age: number;
   isMarried: boolean;
 }
-interface UserContextType{
-    users:User[] | null;
-    addUser: (user:User) => void;
+interface UserContextType {
+  users: User[] | null;
+  addUser: (user: User) => void;
+  updateUser: (id: string) => void;
+  DeleteUser: (id: string) => void;
 }
+const contextInitailValue = {
+  users: null,
+  addUser: () => null,
+  updateUser: () => null,
+  DeleteUser: () => null,
+};
+const UserContext = createContext<UserContextType>(contextInitailValue);
