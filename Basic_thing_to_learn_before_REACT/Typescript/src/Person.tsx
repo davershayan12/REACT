3e00cd6b84cd34ba588e3ceb03a7614f141fda79
Daver Shayan <davershayan13@gmail.com>
2026-03-23 11:47:23 +0500

    .

--- Basic_thing_to_learn_before_REACT/Typescript/src/Person.tsx
+++ Basic_thing_to_learn_before_REACT/Typescript/src/Person.tsx
@@ -8,19 +8,23 @@ interface Props {
 }
 
 export const Person = (props: Props) => {
-    const [isShowInfo, setShowInfo] = useState<boolean>(false);
-    const toggleInfo = () => {
-        setShowInfo((prev) => !prev)
+    const [isShowInfo, setShowInfo] = useState<string | null>(null);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setShowInfo(event.target.value);
     }
+
     return (
         <div>
-            {isShowInfo && (<>
+            {isShowInfo == props.name  && (<>
                 <p>name: {props.name}</p>
                 <p>age: {props.age}</p>
                 <p>This Person: {props.isMarried ? "is married" : "is single"}</p>
             </>)
             }
-            <button onClick={toggleInfo}>Click me</button>
+            <p>{""}
+                {props.name} Bio: { isShowInfo !== props.name ? "Bio not avalible" : isShowInfo}
+            </p>
+            <input onChange={handleChange}></input>
         </div>
     );
 }
