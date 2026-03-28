e7f1a96c60fa0228fb1c57828106aafefb851b6f
Daver Shayan <davershayan13@gmail.com>
2026-03-29 02:03:55 +0500

    .

---
+++ REACT1/02-Conponents/src/Components/Card.tsx
@@ -0,0 +1,9 @@
+function Card(){
+    return(
+    <div className="card">
+        <h1>name1</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel inventore labore culpa cupiditate consequuntur.</p>
+
+    </div>)
+}
+export default Card
