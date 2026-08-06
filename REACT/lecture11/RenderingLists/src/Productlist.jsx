980c4c0a836234ea4b6441ec64d77233554b76fe
Daver Shayan <davershayan13@gmail.com>
2026-08-06 19:21:17 +0500

    .

---
+++ REACT/lecture11/RenderingLists/src/Productlist.jsx
@@ -0,0 +1,7 @@
+export const ProductList = () => {
+  const Product = [{id=1 ,name="laptop", price= 67},{id=2 ,name="Computer", price=99},{id=3 ,name="Tab", price=70}];
+  return;
+  <>
+    <p>ProductList</p>
+  </>;
+};
